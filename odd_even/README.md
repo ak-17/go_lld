4becbd9d2d46297e6a10edaef25bed501a31c4c7
ak-17 <t.akshaya.kumar17@gmail.com>
2025-09-07 11:04:05 +0530

    odd even threads

---
+++ odd_even/README.md
@@ -0,0 +1,3 @@
+## Print Odd and Even Numbers In Different Goroutines
+This Go program demonstrates how to print odd and even numbers using two separate goroutines.
+It uses mutexes and condition variables to synchronize the output between the two goroutines.
---
+++ odd_even/odd_even.go
@@ -0,0 +1,41 @@
+package odd_even
+
+import (
+	"fmt"
+	"sync"
+)
+
+func PrintNumbers(start int, end int, increment int, name string, thisMutex *sync.Mutex, otherMutex *sync.Mutex, group *sync.WaitGroup) {
+	defer group.Done()
+	for i := start; i <= end; i += increment {
+		thisMutex.Lock()
+		fmt.Printf("%s , number: %d\n", name, i)
+		otherMutex.Unlock()
+	}
+}
+
+func PrintEvenOdd(end int) {
+	var oddMutex sync.Mutex
+	var evenMutex sync.Mutex
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	// Locking Odd Mutex to force Event thread to start first
+	oddMutex.Lock()
+	go PrintNumbers(0, end, 2, "even", &evenMutex, &oddMutex, &wg)
+	go PrintNumbers(1, end, 2, "odd", &oddMutex, &evenMutex, &wg)
+	wg.Wait()
+	// Unlocking even Mutex at last, the last printed number mutex to be unlocked
+	// if end is odd, then odd mutex will be locked last
+	if end%2 == 0 {
+		evenMutex.Unlock()
+	} else {
+		oddMutex.Unlock()
+	}
+}
+
+// Usage
+//
+//func main() {
+//	odd_even.PrintEvenOdd(100)
+//}
